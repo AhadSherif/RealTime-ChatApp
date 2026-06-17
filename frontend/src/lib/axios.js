33b256f6e019649f3cc4958f32f772d4c5d3cfee
AhadSherif <132280773+AhadSherif@users.noreply.github.com>
2026-06-17 11:32:33 +0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+      : "https://sherif-chat-api-bvc3hvc8afa7a8ck.centralindia-01.azurewebsites.net/api",
   withCredentials: true,
 });
