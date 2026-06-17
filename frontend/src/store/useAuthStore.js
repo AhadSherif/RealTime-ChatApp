c741d7c1b4a37df760790f6cd60079b996d4bf98
AhadSherif <132280773+AhadSherif@users.noreply.github.com>
2026-06-17 12:25:58 +0400

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://sherif-chat-api-bvc3hvc8afa7a8ck.centralindia-01.azurewebsites.net/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://sherif-chat-api-bvc3hvc8afa7a8ck.centralindia-01.azurewebsites.net";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
