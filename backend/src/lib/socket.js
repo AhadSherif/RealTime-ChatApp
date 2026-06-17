293b9129c90700e4a7a10f6cbffc2d63acc9dada
AhadSherif <132280773+AhadSherif@users.noreply.github.com>
2026-06-17 12:05:26 +0400

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://gentle-forest-0b7e26f10.7.azurestaticapps.net"],
   },
 });
 
